1c15017aa5c884dd2e6d0d559d650ae835eae491
Lisa Berg <lisa.bergmohn@gmail.com>
2025-02-18 08:30:11 +0100

    Lagt till dynamisk GA-skriptinjektion i App, ny build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import About from "./pages/About.jsx";
 import Book from "./pages/Book.jsx";
 import CostumNavbar from "./components/Navbar.jsx";
@@ -6,17 +7,41 @@ import CustomFooter from "./components/Footer.jsx";
 import styles from './styles/globals.module.css';
 
 function App() {
+  useEffect(() => {
+    // Skapa och lägg till GA-skriptet
+    const gaScript = document.createElement("script");
+    gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-WDRB1SGLTV";
+    gaScript.async = true;
+    document.head.appendChild(gaScript);
+
+    // Skapa och lägg till inline GA-skriptet
+    const inlineScript = document.createElement("script");
+    inlineScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){ dataLayer.push(arguments); }
+      gtag("js", new Date());
+      gtag("config", "G-WDRB1SGLTV");
+    `;
+    document.head.appendChild(inlineScript);
+
+    // Städa upp när komponenten unmountas
+    return () => {
+      document.head.removeChild(gaScript);
+      document.head.removeChild(inlineScript);
+    };
+  }, []);
+
   return (
     <Router>
       <div className={styles.wrapper}>
-      <CostumNavbar />
-      <main className={styles.main}>
-      <Routes>
-        <Route path="/" element={<Book />} />
-        <Route path="/About" element={<About />} />
-      </Routes>
-      </main>
-      <CustomFooter />
+        <CostumNavbar />
+        <main className={styles.main}>
+          <Routes>
+            <Route path="/" element={<Book />} />
+            <Route path="/About" element={<About />} />
+          </Routes>
+        </main>
+        <CustomFooter />
       </div>
     </Router>
   );
